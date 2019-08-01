ba7763747dd6f16e541d2d8460898b9023ddc464
hanbin choi <wkghskak@naver.com>
2019-08-01 20:04:33 +0900

    android_190801

---
+++ MyFragment/app/src/main/java/com/example/fragment/MainFragment.java
@@ -0,0 +1,45 @@
+package com.example.fragment;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class MainFragment extends Fragment {
+
+    MainActivity activity;
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        activity = (MainActivity)getActivity(); //메인액티비티 참조
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+
+        activity=null;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.fragment_main,container,false);
+
+        Button button = (Button)rootView.findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                activity.onFragmentChange(1);
+            }
+        });
+
+        return rootView;
+    }
+}
