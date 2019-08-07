c939255c63ad49cd0d6e5129636a91a63d3bde1b
hanbin choi <wkghskak@naver.com>
2019-08-07 10:33:54 +0900

    android_190807

---
+++ MyPager/app/src/main/java/com/example/pager/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.pager;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ViewPager pager = (ViewPager)findViewById(R.id.viewPager);
+        pager.setOffscreenPageLimit(3);
+
+        MoviePagerAdapter adapter = new MoviePagerAdapter(getSupportFragmentManager());
+
+        Fragment1 fragment1 = new Fragment1();
+        adapter.addItem(fragment1);
+
+        Fragment2 fragment2 = new Fragment2();
+        adapter.addItem(fragment2);
+
+        Fragment3 fragment3 = new Fragment3();
+        adapter.addItem(fragment3);
+
+        pager.setAdapter(adapter);
+
+    }
+
+    class MoviePagerAdapter extends FragmentStatePagerAdapter{
+
+        ArrayList<Fragment> items = new ArrayList<Fragment>();
+
+        public MoviePagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        public void addItem(Fragment fragment){
+            items.add(fragment);
+        }
+
+        @Override
+        public Fragment getItem(int i) {
+            return items.get(i);
+        }
+
+        @Override
+        public int getCount() {
+            return items.size();
+        }
+    }
+
+}
